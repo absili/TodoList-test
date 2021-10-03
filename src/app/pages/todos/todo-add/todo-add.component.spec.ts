91ebf819925ace9cb08594dfce409f555b802338
Azmi <bsili.dsi@gmail.com>
2021-10-03 19:59:39 +0200

    fix bug test unit - inject form build

--- src/app/pages/todos/todo-add/todo-add.component.spec.ts
+++ src/app/pages/todos/todo-add/todo-add.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoAddComponent } from './todo-add.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { FormBuilder } from '@angular/forms';
 
 describe('TodoAddComponent', () => {
   let component: TodoAddComponent;
@@ -8,6 +10,8 @@ describe('TodoAddComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      
+      providers: [provideMockStore({}), FormBuilder],
       declarations: [ TodoAddComponent ]
     })
     .compileComponents();
